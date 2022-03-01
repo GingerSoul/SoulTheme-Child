384dd73508eb448299a50d24194b72308e3ff086
Dave Bloom <gingersoulrecords@gmail.com>
2022-03-01 17:28:38 -0500

    precocial

--- functions.php
+++ functions.php
@@ -1,2 +1,8 @@
 <?php
 //* Code goes here
+
+add_filter( 'auth_cookie_expiration', 'keep_me_logged_in_for_1_year' );
+
+function keep_me_logged_in_for_1_year( $expirein ) {
+    return 31556926; // 1 year in seconds
+}
